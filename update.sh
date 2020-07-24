16c4b30c6a21992719a90a33b0037d8c66a1ca75
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-07-24 09:44:35 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,28 @@
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+clear
+echo -e "$red 
+
+██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗
+██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  
+██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  
+╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗
+ ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝"
+ cd $HOME
+ cd WE_ARE_OP
+ cd update
+ git clone https://github.com/Mr-YENOJ/update
+ cd $HOME
+ rm -rf .gift .giftt
+ cd WE_ARE_OP/update/update
+ cp -r .gift .giftt $HOME
+ cd $HOME
+ cd WE_ARE_OP
+ clear
+ php Thanku.php
